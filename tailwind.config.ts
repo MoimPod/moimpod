ffe33d2c1256101aef3b0e511ada3694bf7f0874
dohyeons <ddhhss0603@gmail.com>
2025-02-06 14:43:22 +0900

    feat: heart-scale-up 애니메이션 구현 #16

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,19 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        "heart-scale-up": "heart-scale-up 0.5s cubic-bezier(.77,1.74,.97,1.65)",
+      },
+      keyframes: {
+        "heart-scale-up": {
+          "0%": { transform: "scale(0.8)" },
+          "50%": { transform: "scale(1.3)", fill: "#dc2626" },
+          "100%": {
+            transform: "scale(1)",
+            fill: "#dc2626",
+          },
+        },
+      },
     },
   },
   plugins: [],
