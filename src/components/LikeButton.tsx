b8a63f0f8bf034df28adea1ab0397e72aab80297
dohyeons <ddhhss0603@gmail.com>
2025-02-06 16:50:02 +0900

    feat: LikeButton에 모임이 취소, 마감되었을 경우 props로 받아 조건부 렌더링 구현 #16

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import Heart from "@/images/heart.svg";
+import Bye from "@/images/bye.svg";
 import { useState } from "react";
 
 type LikeButtonProps = {
   onClick: () => void;
+  isClosed: boolean;
 };
 
 export default function LikeButton({ onClick, ...rest }: LikeButtonProps) {
+  const { isClosed } = rest;
   const [clicked, setClicked] = useState(false);
   const handleClick = () => {
     setClicked(!clicked);
@@ -13,14 +18,21 @@ export default function LikeButton({ onClick, ...rest }: LikeButtonProps) {
   };
   return (
     <button
-      className={`flex size-12 items-center justify-center rounded-full border-2 transition-all ${clicked ? "border-0 bg-orange-50" : "bg-white"} border-gray-200`}
+      className={`group flex ${isClosed ? "h-9 w-full max-w-[116px] sm:size-12" : "size-12"} items-center justify-center rounded-full ${clicked || isClosed ? "border-0 bg-orange-50" : "border-2 bg-white"} border-gray-200`}
       onClick={handleClick}
     >
-      <Heart
-        fill="#fff"
-        stroke="#9CA3AF"
-        className={`transition-all ${clicked ? "animate-heart-scale-up fill-red-600 stroke-none" : ""}`}
-      />
+      {!isClosed ? (
+        <Heart
+          fill="#fff"
+          stroke="#9CA3AF"
+          className={`transition-all ${clicked ? "animate-heart-scale-up fill-red-600 stroke-none" : ""}`}
+        />
+      ) : (
+        <>
+          <Bye className="group-hover:animate-hand-wave" />
+          <span className="text-xs text-orange-600 sm:hidden">모임 보내주기</span>
+        </>
+      )}
     </button>
   );
 }
