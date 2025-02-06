fdd158fb8706bb767b4607ef9fbe76030a212a64
dohyeons <ddhhss0603@gmail.com>
2025-02-06 17:28:32 +0900

    feat: 주석 추가 및 삼항연산자 순서 변경 #16

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -10,6 +10,7 @@ type LikeButtonProps = {
 };
 
 export default function LikeButton({ onClick, ...rest }: LikeButtonProps) {
+  // 모임이 마감, 취소된 경우
   const { isClosed } = rest;
   const [clicked, setClicked] = useState(false);
   const handleClick = () => {
@@ -21,17 +22,18 @@ export default function LikeButton({ onClick, ...rest }: LikeButtonProps) {
       className={`group flex ${isClosed ? "h-9 w-full max-w-[116px] sm:size-12" : "size-12"} items-center justify-center rounded-full ${clicked || isClosed ? "border-0 bg-orange-50" : "border-2 bg-white"} border-gray-200`}
       onClick={handleClick}
     >
-      {!isClosed ? (
+      {/* 모임이 마감, 취소된 경우 Bye와 span을, 아닌 경우 Heart를 렌더링  */}
+      {isClosed ? (
+        <>
+          <Bye className="group-hover:animate-hand-wave" />
+          <span className="text-xs text-orange-600 sm:hidden">모임 보내주기</span>
+        </>
+      ) : (
         <Heart
           fill="#fff"
           stroke="#9CA3AF"
           className={`transition-all ${clicked ? "animate-heart-scale-up fill-red-600 stroke-none" : ""}`}
         />
-      ) : (
-        <>
-          <Bye className="group-hover:animate-hand-wave" />
-          <span className="text-xs text-orange-600 sm:hidden">모임 보내주기</span>
-        </>
       )}
     </button>
   );
