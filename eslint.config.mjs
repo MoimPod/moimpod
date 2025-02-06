9276e6f42eeb3af7a575fe638b9f982102e1096b
suzinxix <suzinxix@gmail.com>
2025-02-06 16:15:00 +0900

    chore: eslint config 파일 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,16 @@
-// ESM 기반 ESLint 설정 파일
 import js from "@eslint/js";
-import ts from "@typescript-eslint/eslint-plugin";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import next from "eslint-plugin-next";
-import prettier from "eslint-plugin-prettier";
-import tailwind from "eslint-plugin-tailwindcss";
-import build from "next/dist/build";
+import prettierPlugin from "eslint-plugin-prettier";
+import tailwindPlugin from "eslint-plugin-tailwindcss";
 
 export default [
-  js.configs.recommended, // JavaScript 기본 권장 설정
-  ts.configs.recommended, // TypeScript 권장 설정
+  js.configs.recommended,
   {
-    files: ["**/*.ts", "**/*.tsx"],
-    ignores: ["node_modules", ".next", "public/", "dist/", "build/", ".husky/", "**/*.config.js"],
+    files: ["src/**/*.tsx"],
+    ignores: ["**/*.test.ts", "**/*.test.tsx", ".config.*", ".next/**", "node_modules/*","build/**/*"],
     languageOptions: {
-      parser: tsParser, // TypeScript 파서 적용
+      parser: tsParser,
       parserOptions: {
         project: "./tsconfig.json",
         tsconfigRootDir: import.meta.dirname,
@@ -22,18 +18,15 @@ export default [
       sourceType: "module",
     },
     plugins: {
-      "@typescript-eslint": ts,
-      prettier,
-      tailwind,
-      next,
+      "@typescript-eslint": tsPlugin,
+      prettier: prettierPlugin,
+      tailwindcss: tailwindPlugin,
     },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
       "prettier/prettier": "error",
-      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
       "no-console": "warn",
-      "no-unused-vars": "warn", // 사용하지 않는 변수 경고
-      "no-var": "error", // var 사용 금지 (let, const 사용)
-      "prefer-const": "error", // 가능하면 const 사용
       "tailwindcss/no-custom-classname": "off",
       "tailwindcss/classnames-order": "warn",
     },
