f3552e2eda77dc5db18eed55c5f0c364232f6103
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2025-02-06 14:51:24 +0900

    feat: husky 파일 수정

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,36 +3,47 @@
 import React from "react";
 import Image from "next/image";
 import ProgressBar from "./ProgressBar";
+import ChipInfo from "./ChipInfo";
+import testCard from "./testCard.png";
 
 type CardProps = {
-  image: string;
   name: string;
   dateTime: string;
+  registrationEnd: string;
   location: string;
   participantCount: number;
   capacity: number;
 };
 
-export default function Card({ image, name, dateTime, location, participantCount, capacity }: CardProps) {
+export default function Card({ name, location, dateTime, registrationEnd, participantCount, capacity }: CardProps) {
   const progress = capacity > 0 ? (participantCount / capacity) * 100 : 0;
 
   return (
-    <div className="flex items-center gap-4 rounded-lg border p-4 shadow">
+    <div className="m-3 items-center gap-4 rounded-3xl border-0 p-4 shadow md:flex lg:flex">
       {/* 카드 이미지 */}
-      <Image src={image} alt={name} width={96} height={96} className="rounded-lg object-cover" />
+      <Image
+        src={testCard}
+        alt={"test 이미지"}
+        width={280}
+        height={156}
+        className="rounded-t-lg border-2 md:rounded-l-lg lg:rounded-l-lg"
+      />
       {/* 카드 내용 */}
       <div className="flex-1">
-        <h2 className="text-lg font-bold">{name}</h2>
-        <p className="text-sm text-gray-500">
-          {new Date(dateTime).toLocaleDateString()} | {new Date(dateTime).toLocaleTimeString()}
-        </p>
-        <p className="text-sm text-gray-500">{location}</p>
+        <div className="flex items-center gap-2">
+          <h2 className="text-lg font-bold">{name} |</h2>
+          <p className="text-sm text-gray-500">{location}</p>
+        </div>
+        <ChipInfo dateTime={dateTime} registrationEnd={registrationEnd} />
+
         <p className="text-sm text-gray-500">
           {participantCount}/{capacity}
         </p>
-        <ProgressBar progress={progress} />
+        <div className="flex items-center gap-8">
+          <ProgressBar progress={progress} />
+          <button className="whitespace-nowrap font-semibold text-orange-500">Join Now</button>
+        </div>
       </div>
-      <button className="font-bold text-orange-500">Join Now</button>
     </div>
   );
 }
--- src/components/Card/ProgressBar.tsx
+++ src/components/Card/ProgressBar.tsx
@@ -6,8 +6,8 @@ type ProgressBarProps = {
 
 export default function ProgressBar({ progress }: ProgressBarProps) {
   return (
-    <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
-      <div className="h-2.5 rounded-full bg-blue-600" style={{ width: `${progress}%` }}></div>
+    <div className="h-1.5 w-full rounded-full bg-orange-100 dark:bg-gray-700">
+      <div className="h-1.5 rounded-full bg-orange-600" style={{ width: `${progress}%` }}></div>
     </div>
   );
 }
