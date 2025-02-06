cf0e88f0f130278241d54e818144c260acf47e62
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2025-02-06 15:54:27 +0900

    feat: 모임 마감 알림 Tag 컴포넌트 생성, Card 컴포넌트 style 수정

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import ProgressBar from "./ProgressBar";
 import ChipInfo from "./ChipInfo";
+import Tag from "./Tag";
 import testCard from "./testCard.png";
 
 type CardProps = {
@@ -19,29 +20,36 @@ export default function Card({ name, location, dateTime, registrationEnd, partic
   const progress = capacity > 0 ? (participantCount / capacity) * 100 : 0;
 
   return (
-    <div className="m-3 items-center gap-4 rounded-3xl border-0 p-4 shadow md:flex lg:flex">
+    <div className="m-3 items-center rounded-3xl border-0 shadow md:flex md:h-[156px] lg:flex lg:h-[156px]">
       {/* 카드 이미지 */}
-      <Image
-        src={testCard}
-        alt={"test 이미지"}
-        width={280}
-        height={156}
-        className="rounded-t-lg border-2 md:rounded-l-lg lg:rounded-l-lg"
-      />
+      <div className="relative">
+        <Tag text="오늘 21시 마감" />
+        <Image
+          src={testCard}
+          alt={"test 이미지"}
+          width={280}
+          height={156}
+          className="h-[156px] w-full rounded-t-3xl border-2 md:w-[280px] md:rounded-l-3xl lg:w-[280px] lg:rounded-l-3xl"
+        />
+      </div>
+
       {/* 카드 내용 */}
-      <div className="flex-1">
-        <div className="flex items-center gap-2">
+      <div className="flex-1 pb-3 pl-6 pr-6 pt-4">
+        {/* 카드 제목 */}
+        <div className="mb-2 flex items-center gap-2">
           <h2 className="text-lg font-bold">{name} |</h2>
           <p className="text-sm text-gray-500">{location}</p>
+          <p className="ml-auto">❤️</p>
         </div>
         <ChipInfo dateTime={dateTime} registrationEnd={registrationEnd} />
 
-        <p className="text-sm text-gray-500">
+        {/* 카드 인원 정보 */}
+        <p className="mt-5 text-sm text-gray-500">
           {participantCount}/{capacity}
         </p>
-        <div className="flex items-center gap-8">
+        <div className="mb-3 flex items-center gap-8">
           <ProgressBar progress={progress} />
-          <button className="whitespace-nowrap font-semibold text-orange-500">Join Now</button>
+          <button className="whitespace-nowrap font-semibold text-orange-500">join now</button>
         </div>
       </div>
     </div>
