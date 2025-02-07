8624006e6c83ddd2a153f416f73209030940bc54
GODdaeyoon <105477832+GODdaeyoon@users.noreply.github.com>
2025-02-07 09:29:22 +0900

    feat:pretendard font 최적화 #23

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,20 @@
 import Header from "@/components/Header";
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+import React from "react";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "MoimPod: 취미 모임 플랫폼",
   description: "취미 모임 플랫폼",
 };
+
+const pretendard = localFont({
+  src: "../../public/font/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
+      <body className={`${pretendard.variable} font-pretendard`}>
         <div>
           <Header />
           <main>{children}</main>
