a2c0e9aa3b711ca1273edffd019a1fdda725c31f
suzinxix <suzinxix@gmail.com>
2025-02-07 15:43:47 +0900

    fix: 스타일 중복 코드 수정

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -4,8 +4,10 @@ type SpinnerProps = {
 
 export default function Spinner({ color = "gray" }: SpinnerProps) {
   const spinnerColors = {
-    white: "border-white border-t-transparent",
-    gray: "border-gray-400 border-t-transparent",
+    white: "border-white",
+    gray: "border-gray-400",
   };
-  return <span className={`size-5 animate-spin rounded-full border-2 ${spinnerColors[color]}`}></span>;
+  return (
+    <span className={`size-5 animate-spin rounded-full border-2 border-t-transparent ${spinnerColors[color]}`}></span>
+  );
 }
