a9e81eed1952ebefa82c289c90e30c1705d96fbc
suzinxix <suzinxix@gmail.com>
2025-02-07 13:09:21 +0900

    fix: 타입 null 추가

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,3 +1,3 @@
-export function cn(...classes: (string | undefined | boolean)[]) {
+export function cn(...classes: (string | undefined | boolean | null)[]) {
   return classes.filter(Boolean).join(" ");
 }
