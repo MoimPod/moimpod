aedbbef0a425482a50270e954ab8b7e2b255a3f5
suzinxix <suzinxix@gmail.com>
2025-02-07 16:29:30 +0900

    feat: 백드롭 props 추가

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,10 +6,11 @@ import Close from "@/images/close.svg";
 type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
+  closeOnBackdropClick?: boolean;
   children: React.ReactNode;
 };
 
-export default function Modal({ isOpen, onClose, children }: ModalProps) {
+export default function Modal({ isOpen, onClose, children, closeOnBackdropClick = false }: ModalProps) {
   useEffect(() => {
     document.body.style.overflow = isOpen ? "hidden" : "";
   }, [isOpen]);
@@ -19,7 +20,14 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
   }
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+      onClick={(e) => {
+        if (e.target === e.currentTarget && closeOnBackdropClick) {
+          onClose();
+        }
+      }}
+    >
       <div className="relative rounded-lg bg-white p-[24px] shadow-lg">
         <button className="absolute right-[24px] top-[24px] cursor-pointer" onClick={onClose}>
           <Close />
