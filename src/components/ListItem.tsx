bbcf55e036a9ee11be5c138a4cd8bd7e5887dd7e
dohyeons <ddhhss0603@gmail.com>
2025-02-07 15:03:34 +0900

    feat: ListItem 컴포넌트 하드코딩 구현 #24

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,11 +1,46 @@
 import Image from "next/image";
 import testImg from "@/images/auth_main_img.png";
+import Check from "@/images/check.svg";
+import Person from "@/images/person.svg";
+import Button from "@/components/Button";
 
 export default function ListItem() {
   return (
-    <div>
-      <div>
-        <Image src={testImg} alt="모임 이미지" width={280} height={156} className="w-full max-w-[311px] rounded-3xl" />
+    <div className="flex w-full flex-col items-stretch gap-4 md:flex-row">
+      <Image
+        src={testImg}
+        alt="모임 이미지"
+        width={280}
+        height={156}
+        className="h-[156px] w-full max-w-[311px] rounded-3xl md:w-[280px]"
+      />
+      {/* 모임 정보 **/}
+      <div className="flex flex-col">
+        <div className="mb-[11px] flex gap-2">
+          <div className="break-keep rounded-3xl bg-orange-100 px-3 py-[6px] text-sm text-orange-600">이용 예정</div>
+          <div className="flex items-center gap-1 break-keep rounded-3xl border border-orange-100 bg-white px-3 py-[6px] text-sm text-orange-600">
+            <Check />
+            개설 확정
+          </div>
+        </div>
+        <div className="mb-[5px] flex items-center">
+          <span className="text-lg font-semibold">달램핏 오피스 스트레칭 | </span>
+          {/*이 부분 공백{' '}이 린트에 걸려서 &nbsp 로 처리했는데 어떠신가요  **/}
+          &nbsp;
+          <span className="text-sm">을지로 3가</span>
+        </div>
+        <div className="mb-4 flex items-center gap-3 text-sm">
+          <div>1월 7일 · 17:30 </div>
+          <div className="flex items-center gap-[2px] text-gray-700">
+            <Person />
+            <span>20/20</span>
+          </div>
+        </div>
+        <div>
+          <Button className="w-[120px]" size="sm" styleType="outline">
+            예약 취소하기
+          </Button>
+        </div>
       </div>
     </div>
   );
