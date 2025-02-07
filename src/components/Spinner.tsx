d7296b7e5a7a649d3c3a0f1c40bdf0fa6e885581
suzinxix <suzinxix@gmail.com>
2025-02-07 11:13:35 +0900

    feat: Spinner 컴포넌트 구현

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+type SpinnerProps = {
+  color?: "gray" | "white";
+};
+
+export default function Spinner({ color = "gray" }: SpinnerProps) {
+  const spinnerColors = {
+    white: "border-white border-t-transparent",
+    gray: "border-gray-400 border-t-transparent",
+  };
+  return <span className={`h-5 w-5 animate-spin rounded-full border-2 ${spinnerColors[color]}`}></span>;
+}
