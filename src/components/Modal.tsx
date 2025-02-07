979790e395109c400c2223574cebc84e03428ede
suzinxix <suzinxix@gmail.com>
2025-02-07 15:53:51 +0900

    feat: 모달 오픈 시 스크롤 방지

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import Close from "@/images/close.svg";
 
 type ModalProps = {
@@ -9,12 +10,16 @@ type ModalProps = {
 };
 
 export default function Modal({ isOpen, onClose, children }: ModalProps) {
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? "hidden" : "";
+  }, [isOpen]);
+
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div className="z-99 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative rounded-lg bg-white p-[24px] shadow-lg">
         <button className="absolute right-[24px] top-[24px]" onClick={onClose}>
           <Close />
