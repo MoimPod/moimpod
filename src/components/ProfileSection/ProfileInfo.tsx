30f48c4528a91f609d3a24c3cc6c04732351fba5
dohyeons <ddhhss0603@gmail.com>
2025-02-11 17:07:00 +0900

    feat: ProfileInfo 프로필을 Avatar 컴포넌트로 변경 #46

--- src/components/ProfileSection/ProfileInfo.tsx
+++ src/components/ProfileSection/ProfileInfo.tsx
@@ -1,4 +1,4 @@
-import Profile from "@/images/profile.svg";
+import Avatar from "@/components/Avatar";
 
 // 프로필 이미지, 이름, 회사, 이메일 등 사용자 정보를 표시
 type ProfileInfoProps = {
@@ -10,7 +10,9 @@ type ProfileInfoProps = {
 export default function ProfileInfo({ email, name, companyName, imageUrl }: ProfileInfoProps) {
   return (
     <div className="flex w-full flex-col gap-2 pl-24 pt-3">
-      <Profile width="56" height="56" className="absolute left-6 top-[54px]" />
+      <div className="absolute left-6 top-[54px]">
+        <Avatar size={56} />
+      </div>
       <span className="font-semibold text-gray-800">{name}</span>
       <div className="flex flex-col gap-1">
         <div className="text-sm">
