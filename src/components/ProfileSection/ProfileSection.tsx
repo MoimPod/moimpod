d1e11081dd12c208372c2833407dc2df7e63461b
dohyeons <ddhhss0603@gmail.com>
2025-02-11 17:09:21 +0900

    feat: ProfileSection에 주석 추가 #46

--- src/components/ProfileSection/ProfileSection.tsx
+++ src/components/ProfileSection/ProfileSection.tsx
@@ -5,9 +5,10 @@ import ProfileHeader from "@/components/ProfileSection/ProfileHeader";
 import ProfileInfo from "@/components/ProfileSection/ProfileInfo";
 import { useState } from "react";
 
-// 각 하위 컴포넌트를 배치하는 컴포넌트
+// 각 하위 컴포넌트를 배치
 // 모달의 열림, 닫힘 상태를 담당
 export default function ProfileSection() {
+  // 유저 정보를 가져오는 로직 작성 예정
   const [isModal, setIsModal] = useState(false);
   const handleOpen = () => {
     setIsModal(true);
@@ -20,7 +21,7 @@ export default function ProfileSection() {
     <section className="relative w-full rounded-3xl border-2 border-gray-200 bg-white pb-5">
       <ProfileHeader onEditClick={handleOpen} />
       <ProfileEditModal isOpen={isModal} onClose={handleClose} />
-      <ProfileInfo email={"0lYlF@example.com"} name={"이상민"} companyName={"이상민"} imageUrl={""} />
+      <ProfileInfo email={"leesm@example.com"} name={"이상민"} companyName={"이상민컴퍼니"} imageUrl={""} />
     </section>
   );
 }
