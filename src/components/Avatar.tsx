b1e593dbfcfb61e03f1781688dec85a982f6f547
suzinxix <suzinxix@gmail.com>
2025-02-11 15:18:58 +0900

    feat: 프로필 이미지 컴포넌트 구현

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
+type AvatarProps = {
+  imageUrl?: string;
+  size?: number;
+};
+
+export default function Avatar({ imageUrl, size = 30 }: AvatarProps) {
+  const [isError, setIsError] = useState(false);
+  const DEFAULT_IMAGE = "/images/profile.svg";
+
+  return (
+    <div className="relative">
+      <Image
+        src={isError || !imageUrl ? DEFAULT_IMAGE : imageUrl}
+        alt={"프로필 이미지"}
+        width={size}
+        height={size}
+        className="rounded-full"
+        onError={() => setIsError(true)}
+      />
+    </div>
+  );
+}
