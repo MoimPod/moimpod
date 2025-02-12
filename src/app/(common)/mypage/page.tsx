b9c4fb93d262ab889895c05e2320798fa03f7800
dohyeons <ddhhss0603@gmail.com>
2025-02-12 13:23:05 +0900

    feat: mypage 기초 레이아웃 설정 #45

--- src/app/(common)/mypage/page.tsx
+++ src/app/(common)/mypage/page.tsx
@@ -1,3 +1,13 @@
+import ProfileSection from "@/components/ProfileSection/ProfileSection";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-[30px] pt-8">
+      <div className="flex flex-col gap-6">
+        <h2 className="text-2xl font-semibold">마이 페이지</h2>
+        <ProfileSection />
+      </div>
+      <div className="border-t-2 border-gray-900 bg-white px-6 pt-6"></div>
+    </div>
+  );
 }
