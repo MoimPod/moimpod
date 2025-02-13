b248d9162056496ba0849ae8fa10288bc878bfe2
suzinxix <suzinxix@gmail.com>
2025-02-13 18:40:45 +0900

    feat: 리뷰 리스트 불러오는 훅 구현

--- src/app/(common)/gathering/types.ts
+++ src/app/(common)/gathering/types.ts
@@ -1,8 +1,8 @@
 export type User = {
   id: number;
-  email: string;
+  email?: string;
   name: string;
-  companyName: string;
+  companyName?: string;
   image: string | null;
 };
 
@@ -12,4 +12,53 @@ export type GatheringParticipant = {
   gatheringId: number;
   joinedAt: string;
   User: User;
+};
+
+export type ReviewQuery = {
+  gatheringId?: string;
+  useId?: number;
+  type?: string;
+  location?: string;
+  date?: string;
+  registrationEnd?: string;
+  sortBy?: "createdAt" | "score" | "participantCount";
+  sortOrder?: "asc" | "desc";
+  limit?: number;
+  offset?: number;
+};
+
+type Gathering = {
+  teamId: string;
+  id: number;
+  type: string;
+  name: string;
+  dateTime: string;
+  location: string;
+  image: string;
+};
+
+export type Review = {
+  teamId?: string;
+  id: number;
+  score: number;
+  comment: string;
+  createdAt: string;
+  Gathering?: Gathering;
+  User: User;
+};
+
+// 실제 데이터 응답값
+export type ReviewsResponse = {
+  data: Review[];
+  totalItemCount: number;
+  currentPage: number;
+  totalPages: number;
+};
+
+// 리뷰 정제 데이터
+export type ReviewData = {
+  reviews: Review[];
+  totalItemCount: number;
+  currentPage: number;
+  totalPages: number;
 };
