71b6b055c106197e5d77decfac9b32c121e50308
dohyeons <ddhhss0603@gmail.com>
2025-02-13 17:01:09 +0900

    feat: mypage에 탭 배치 #45

--- src/app/(common)/mypage/page.tsx
+++ src/app/(common)/mypage/page.tsx
@@ -1,28 +1,13 @@
 "use client";
 
 import ProfileSection from "@/components/ProfileSection/ProfileSection";
-import { useEffect, useRef, useState } from "react";
-const tabItems = ["나의 모임", "나의 리뷰", "내가 만든 모임"];
+import Tab from "@/components/Tab";
+import CategoryButton from "@/components/CategoryButton";
+
+const tabItems1 = ["나의 모임", "나의 리뷰", "내가 만든 모임"];
+const categories = ["작성 가능한 리뷰", "작성한 리뷰"];
 
 export default function Page() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [sliderStyle, setSliderStyle] = useState({ width: 0, translateX: 0 });
-  const tabRefs = useRef<(HTMLLIElement | null)[]>([]);
-
-  useEffect(() => {
-    if (!tabRefs.current.length) return; // 아직 ref 배열이 비어 있다면 패스
-
-    const activeTab = tabRefs.current[activeIndex];
-    if (activeTab) {
-      const width = activeTab.offsetWidth;
-      // 활성 탭 이전 탭들의 누적 offsetWidth를 계산
-      const offsetLeft = tabRefs.current
-        .slice(0, activeIndex)
-        .reduce((acc, el) => acc + (el?.offsetWidth || 0) + 12, 0);
-      setSliderStyle({ width, translateX: offsetLeft });
-    }
-  }, [activeIndex]);
-
   return (
     <div className="flex flex-col gap-[30px] pt-8">
       <div className="flex flex-col gap-6">
@@ -31,30 +16,22 @@ export default function Page() {
       </div>
       {/* 마이페이지의 콘텐츠 */}
       <div className="border-t-2 border-gray-900 bg-white px-6 pt-6">
-        {/* 탭 */}
-        <div className="relative">
-          <ul className="flex gap-3 text-lg font-semibold text-gray-400">
-            {tabItems.map((tabItem, idx) => (
-              <li
-                onClick={() => setActiveIndex(idx)}
-                key={tabItem}
-                ref={(el) => {
-                  tabRefs.current[idx] = el;
-                }}
-                className={`${activeIndex === idx && "text-gray-900"} mb-1 cursor-pointer transition-colors duration-300`}
-              >
-                {tabItem}
-              </li>
-            ))}
-          </ul>
-          <div
-            style={{
-              width: sliderStyle.width,
-              transform: `translateX(${sliderStyle.translateX}px)`,
-            }}
-            className={`absolute bottom-0 h-[2px] bg-gray-900 transition-all duration-300`}
-          />
-        </div>
+        <Tab
+          categories={
+            <CategoryButton categories={categories}>
+              {categories.map((category) => (
+                <CategoryButton.Title key={category} category={category} />
+              ))}
+            </CategoryButton>
+          }
+          targetIndex={1}
+        >
+          {tabItems1.map((tabItem, idx) => (
+            <Tab.Item key={tabItem} index={idx}>
+              {tabItem}
+            </Tab.Item>
+          ))}
+        </Tab>
       </div>
     </div>
   );
