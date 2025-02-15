a2c11149a32175177d19f6ce7395f91f41e21e7d
suzinxix <suzinxix@gmail.com>
2025-02-15 17:01:43 +0900

    fix: hydration 오류 수정

--- src/stores/useFavoritesStore.ts
+++ src/stores/useFavoritesStore.ts
@@ -3,14 +3,19 @@ import { persist } from "zustand/middleware";
 
 type FavoritesStore = {
   favorites: string[]; // 찜한 모임 id
+  isLoaded: boolean; // zustand 상태가 로드되었는지 확인
+  setLoaded: (value: boolean) => void;
   addFavorite: (id: string) => void;
   removeFavorite: (id: string) => void;
   toggleFavorite: (id: string) => void;
 };
+
 export const useFavoritesStore = create<FavoritesStore>()(
   persist(
     (set, get) => ({
       favorites: [],
+      isLoaded: false,
+      setLoaded: (value: boolean) => set({ isLoaded: value }), // 상태 로드 여부 설정
       addFavorite: (id) => set((state) => ({ favorites: [...state.favorites, id] })),
       removeFavorite: (id) => set((state) => ({ favorites: state.favorites.filter((item) => item !== id) })),
       toggleFavorite: (id) => {
@@ -24,6 +29,14 @@ export const useFavoritesStore = create<FavoritesStore>()(
     }),
     {
       name: "favorites-storage",
+      onRehydrateStorage: () => (state, error) => {
+        if (error) {
+          console.error("Hydration error:", error);
+        } else {
+          console.log("Hydration finished!");
+          state?.setLoaded(true); // 스토어가 로드되었음을 설정
+        }
+      },
     },
   ),
 );
