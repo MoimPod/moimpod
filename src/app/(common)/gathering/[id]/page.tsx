1101e26ceee74b344897d494bc2595977d1509b8
suzinxix <suzinxix@gmail.com>
2025-02-15 17:03:54 +0900

    feat: 로딩 및 에러처리

--- src/app/(common)/gathering/[id]/page.tsx
+++ src/app/(common)/gathering/[id]/page.tsx
@@ -1,7 +1,10 @@
+import { Suspense } from "react";
 import apiClient from "@/services/appClient";
 import Gathering from "../_components/Gathering";
 import Reviews from "../_components/Reviews";
+import Spinner from "@/components/Spinner";
 import type { GatheringParticipantType, GatheringType } from "../types";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id: gatheringId } = await params;
@@ -13,7 +16,12 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
   return (
     <div className="flex flex-col gap-6">
       <Gathering gathering={gathering} profileImages={profileImages} />
-      <Reviews gatheringId={gatheringId} />
+
+      <ErrorBoundary>
+        <Suspense fallback={<Spinner />}>
+          <Reviews gatheringId={gatheringId} />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
