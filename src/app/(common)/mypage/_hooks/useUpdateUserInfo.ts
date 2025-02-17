c0982bbd60a72f52e00afafc0861fe43ff438195
dohyeons <ddhhss0603@gmail.com>
2025-02-17 17:45:09 +0900

    feat: axiosInstance 사용해 useUpdateUserInfo 구현 #80

--- src/app/(common)/mypage/_hooks/useUpdateUserInfo.ts
+++ src/app/(common)/mypage/_hooks/useUpdateUserInfo.ts
@@ -1,19 +1,9 @@
-import apiClient from "@/services/appClient";
+import axiosInstance from "@/utils/axiosInstance";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 
 const updateUserInfo = async (formData: FormData) => {
   try {
-    const { data } = await apiClient.put(
-      `/auths/user`,
-      formData,
-      // 임시로 설정하는 헤더. 추후에 axiosInstance 설정되면 바꾸기.
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWFtSWQiOiIzIiwidXNlcklkIjoxMzQzLCJpYXQiOjE3Mzk1MjAzNTgsImV4cCI6MTczOTUyMzk1OH0.fCUB_NlrA2oJ6QZsM1zz2yUPI0twhm3Ttxv6dlpFWgo`,
-        },
-      },
-    );
+    const { data } = await axiosInstance.put(`/auths/user`, formData);
     return data;
   } catch (error) {
     throw new Error("데이터를 불러오지 못했습니다.");
