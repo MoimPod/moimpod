1d35187243cb62f955aa5b9938dad98e32439925
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2025-02-17 16:33:53 +0900

    feat: 모임 없을 시 화면

--- src/app/_home/CardList.tsx
+++ src/app/_home/CardList.tsx
@@ -25,7 +25,7 @@ export type CardData = {
 };
 export default function CardList() {
   // 원본 데이터
-  const { data: cards = [], isLoading, error } = useFetchGatherings();
+  const { data: cards = [], isLoading, error } = [];
   // 정렬된 데이터
   const [sortedCards, setSortedCards] = useState<CardData[]>([]);
 
@@ -103,9 +103,14 @@ export default function CardList() {
           </div>
           <SortButton cards={cards} onSort={setSortedCards} />
         </div>
-        {sortedCards.map((card) => (
-          <Card key={card.id} {...card} registrationEnd={card.registrationEnd ?? ""} />
-        ))}
+        {sortedCards.length === 0 ? (
+          <div className="flex h-[calc(100vh-300px)] flex-col items-center justify-center text-center text-sm font-medium text-gray-500">
+            <p>아직 모임이 없어요</p>
+            <p className="mt-2">지금 바로 모임을 만들어보세요</p>
+          </div>
+        ) : (
+          sortedCards.map((card) => <Card key={card.id} {...card} registrationEnd={card.registrationEnd ?? ""} />)
+        )}
       </div>
 
       <CreateGatheringsModal isOpen={isModalOpen} onClose={handleClose} />
