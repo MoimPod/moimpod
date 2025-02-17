fba7bfd4eb2f0c4f68ea7c4f3e5afe425844cc8d
suzinxix <suzinxix@gmail.com>
2025-02-17 16:42:35 +0900

    fix: 타입 수정

--- src/app/(common)/reviews/types.ts
+++ src/app/(common)/reviews/types.ts
@@ -2,13 +2,13 @@ const GATHERING_TYPE = ["DALLAEMFIT", "OFFICE_STRETCHING", "MINDFULNESS", "WORKA
 
 export type ScoresQuery = {
   gatheringId?: number;
-  type?: typeof GATHERING_TYPE;
+  type?: (typeof GATHERING_TYPE)[number];
 };
 
 export type ScoresResponse = {
   teamId: number;
   gatheringId: number;
-  type?: typeof GATHERING_TYPE;
+  type?: (typeof GATHERING_TYPE)[number];
   averageScore: number;
   oneStar: number;
   twoStars: number;
