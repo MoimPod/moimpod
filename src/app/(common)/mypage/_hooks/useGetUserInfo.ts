4ef03f5abdf298e0f2a8dae128fa995650da5ed6
dohyeons <ddhhss0603@gmail.com>
2025-02-17 17:43:28 +0900

    feat: axiosInstance 사용해 useGetUserInfo 구현 #45

--- src/app/(common)/mypage/_hooks/useGetUserInfo.ts
+++ src/app/(common)/mypage/_hooks/useGetUserInfo.ts
@@ -1,18 +1,9 @@
-import apiClient from "@/services/appClient";
+import axiosInstance from "@/utils/axiosInstance";
 import { useQuery } from "@tanstack/react-query";
 
 const fetchUserInfo = async () => {
   try {
-    const { data } = await apiClient.get(
-      `/auths/user`,
-      // 임시로 설정하는 헤더. 추후에 axiosInstance 설정되면 바꾸기.
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZWFtSWQiOiIzIiwidXNlcklkIjoxMzQzLCJpYXQiOjE3Mzk1MjAzNTgsImV4cCI6MTczOTUyMzk1OH0.fCUB_NlrA2oJ6QZsM1zz2yUPI0twhm3Ttxv6dlpFWgo`,
-        },
-      },
-    );
+    const { data } = await axiosInstance.get("auths/user");
     return data;
   } catch (error) {
     throw new Error("데이터를 불러오지 못했습니다.");
