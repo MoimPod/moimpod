a3ce80f0f2a0b025e5b2cbb96265f05781836fce
dohyeons <ddhhss0603@gmail.com>
2025-02-19 00:10:07 +0900

    fix: LikeButton 반응형 수정 #16

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -19,7 +19,7 @@ export default function LikeButton({ onClick, isClosed, isLiked, className, ...r
   };
   return (
     <button
-      className={`group flex ${isClosed ? "h-9 w-full max-w-[116px] sm:size-12" : "size-12"} items-center justify-center rounded-full ${isClicked || isClosed ? "border-0 bg-orange-50" : "border-2 border-gray-200 bg-white"} ${className}`}
+      className={`group flex ${isClosed ? "h-9 w-full max-w-[116px] md:size-12" : "size-12"} items-center justify-center rounded-full ${isClicked || isClosed ? "border-0 bg-orange-50" : "border-2 border-gray-200 bg-white"} ${className}`}
       onClick={handleClick}
       {...rest}
     >
@@ -27,7 +27,7 @@ export default function LikeButton({ onClick, isClosed, isLiked, className, ...r
       {isClosed ? (
         <>
           <Bye className="group-hover:animate-hand-wave" />
-          <span className="text-xs text-orange-600 sm:hidden">모임 보내주기</span>
+          <span className="text-xs text-orange-600 md:hidden">모임 보내주기</span>
         </>
       ) : (
         <Heart
