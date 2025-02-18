3418f0cde8e8e2e715a9fe8e77b73f2fe5b641dc
dohyeons <ddhhss0603@gmail.com>
2025-02-18 14:07:43 +0900

    feat: MypageContent 상수 이름 변경 및 리스트 컨테이너에 flex-1 적용 #45

--- src/components/MypageContent.tsx
+++ src/components/MypageContent.tsx
@@ -3,29 +3,29 @@
 import CategoryButton from "@/components/CategoryButton";
 import Tab from "@/components/Tab";
 
-const tabItems1 = ["나의 모임", "나의 리뷰", "내가 만든 모임"];
-const categories = ["작성 가능한 리뷰", "작성한 리뷰"];
+const TAB_ITEMS = ["나의 모임", "나의 리뷰", "내가 만든 모임"];
+const CATEGORIES = ["작성 가능한 리뷰", "작성한 리뷰"];
 
 export default function MypageContent() {
   return (
     <div className="flex flex-1 flex-col gap-6 border-t-2 border-gray-900 bg-white px-6 pt-6">
       <Tab
         category={
-          <CategoryButton categories={categories}>
-            {categories.map((category) => (
+          <CategoryButton categories={CATEGORIES}>
+            {CATEGORIES.map((category) => (
               <CategoryButton.Title key={category} category={category} />
             ))}
           </CategoryButton>
         }
         targetIndex={1}
       >
-        {tabItems1.map((tabItem, idx) => (
+        {TAB_ITEMS.map((tabItem, idx) => (
           <Tab.Item key={tabItem} index={idx}>
             {tabItem}
           </Tab.Item>
         ))}
       </Tab>
-      <div className="border">리스트 컨테이너</div>
+      <div className="flex-1 border">리스트 컨테이너</div>
     </div>
   );
 }
