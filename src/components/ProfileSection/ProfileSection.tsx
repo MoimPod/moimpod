ff82e4132e5e6e2bd846ba84a4661342d852faf9
dohyeons <ddhhss0603@gmail.com>
2025-02-18 14:06:10 +0900

    feat: ProfileEditModal에 props로 companyName전달 #80

--- src/components/ProfileSection/ProfileSection.tsx
+++ src/components/ProfileSection/ProfileSection.tsx
@@ -34,7 +34,7 @@ export default function ProfileSection() {
   return (
     <section className="relative w-full rounded-3xl border-2 border-gray-200 bg-white pb-5">
       <ProfileHeader onEditClick={handleOpen} />
-      <ProfileEditModal isOpen={isModal} onClose={handleClose} imageUrl={data?.image} />
+      <ProfileEditModal isOpen={isModal} onClose={handleClose} companyName={data?.companyName} imageUrl={data?.image} />
       <ProfileInfo email={data?.email} name={data?.name} companyName={data?.companyName} imageUrl={data?.image} />
     </section>
   );
