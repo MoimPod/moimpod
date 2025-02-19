f5d150981d302e73fc41abbb47c75d7ed6dfce74
suzinxix <suzinxix@gmail.com>
2025-02-19 15:45:30 +0900

    feat: 모임에 관한 인터렉션이 가능한 FloatingBar 생성

--- src/app/(common)/gathering/[id]/page.tsx
+++ src/app/(common)/gathering/[id]/page.tsx
@@ -24,7 +24,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
         </Suspense>
       </ErrorBoundary>
 
-      <FloatingBar />
+      <FloatingBar gatheringId={gatheringId} hostUserId={gathering.createdBy} />
     </div>
   );
 }
