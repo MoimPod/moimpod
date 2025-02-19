dd26d1cc79fbf36efd0c74e472d9ae48344c6323
dohyeons <ddhhss0603@gmail.com>
2025-02-19 11:36:22 +0900

    feat: ListItem에 InactiveLayer 조건부 렌더링 #108

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,20 +1,29 @@
 import Image from "next/image";
-import Heart from "@/images/heart.svg";
 import Profile from "@/images/profile.svg";
 import CapacityStatus from "@/components/CapacityStatus";
 import { cn } from "@/utils/classnames";
 import Check from "@/images/check.svg";
 import { PropsWithChildren } from "react";
 import formatDateToYYYYMMDD from "@/utils/formatDateToYYYYMMDD";
+import InactiveLayer from "@/components/InactiveLayout";
 
 type ListItemProps = {
   CardImage?: React.ReactNode;
+  canceledAt?: string;
+  handleCancel?: () => void;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 // ListItem의 템플릿 컴포넌트
-export default function ListItem({ children, CardImage, className }: PropsWithChildren<ListItemProps>) {
+export default function ListItem({
+  children,
+  CardImage,
+  canceledAt,
+  handleCancel,
+  className,
+}: PropsWithChildren<ListItemProps>) {
   return (
-    <div className={`flex w-full flex-col items-stretch gap-4 border-gray-300 py-6 md:max-w-none md:flex-row`}>
+    <div className={`relative flex w-full flex-col items-stretch gap-4 border-gray-300 md:max-w-none md:flex-row`}>
+      {canceledAt && handleCancel && <InactiveLayer onClick={handleCancel} message="모집 취소된 모임이에요" />}
       {CardImage}
       <div className={`flex flex-col ${className}`}>{children}</div>
     </div>
