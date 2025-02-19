3e34c2c95bb5cfc40f4d644ed4868a7e2f10f13e
suzinxix <suzinxix@gmail.com>
2025-02-19 15:40:44 +0900

    feat: 로그인 모달 구현

---
+++ src/components/LoginModal.tsx
@@ -0,0 +1,22 @@
+import Modal from "@/components/Modal";
+import Button from "@/components/Button";
+import { useRouter } from "next/navigation";
+
+type LoginModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
+  const router = useRouter();
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} className="w-full max-w-[450px]">
+      <div className="flex h-[151px] w-full flex-col items-center justify-between font-medium">
+        <div className="flex flex-1 items-center">로그인이 필요합니다.</div>
+        <Button onClick={() => router.push("/sign-in")} className="w-32">
+          확인
+        </Button>
+      </div>
+    </Modal>
+  );
+}
