657acbf5c6c9cddbb07485e4a011d39d5aa26bb1
GODdaeyoon <105477832+GODdaeyoon@users.noreply.github.com>
2025-02-19 18:37:36 +0900

    fix:app layout과 common layout 변경, header token값에 따른 로그인과 프로필 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Header from "@/components/Header";
 import CardList from "./_home/CardList";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <CardList />
     </div>
   );
