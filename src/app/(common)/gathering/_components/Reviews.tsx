dd72af7cccbbb9d58241e943268c06abc7e37901
suzinxix <suzinxix@gmail.com>
2025-02-20 17:58:08 +0900

    feat: 리뷰 목록 페이지네이션과 필터링 구현

--- src/app/(common)/gathering/_components/Reviews.tsx
+++ src/app/(common)/gathering/_components/Reviews.tsx
@@ -1,17 +1,45 @@
 "use client";
 
-import { useState } from "react";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import ReviewList from "../_components/ReviewList";
 import Pagination from "../_components/Pagination";
-import { useGetReviews } from "@/app/(common)/gathering/_hooks/useGetReviews";
+import { useGetReviews } from "../_hooks/useGetReviews";
+import { useQueryParams } from "@/hooks/useQueryParams";
 
 export default function Reviews({ gatheringId }: { gatheringId: string }) {
-  const [current, setCurrentPage] = useState(1);
+  const paramsObj = useQueryParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const sortOrder = searchParams.get("sortOrder") || "latest";
 
-  const { data } = useGetReviews(gatheringId);
+  const { data } = useGetReviews(gatheringId, paramsObj);
 
   const handlePageChange = (page: number) => {
-    setCurrentPage(page);
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+
+    const limit = 2;
+    const offset = (page - 1) * limit;
+
+    newSearchParams.set("offset", offset.toString());
+    newSearchParams.set("limit", limit.toString()); // limit도 유지
+
+    router.push(`${pathname}?${newSearchParams.toString()}`);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (value === "latest") {
+      params.delete("sortBy");
+      params.delete("sortOrder");
+    } else {
+      params.set("sortBy", "score");
+      params.set("sortOrder", value);
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
   };
 
   if (data.reviews.length === 0) {
@@ -20,6 +48,16 @@ export default function Reviews({ gatheringId }: { gatheringId: string }) {
 
   return (
     <div className="bg-white p-6">
+      <div className="flex justify-between">
+        <h1 className="text-lg font-semibold">이용자들은 이 프로그램을 이렇게 느꼈어요!</h1>
+
+        <select value={sortOrder} onChange={handleChange}>
+          <option value="latest">최신순</option>
+          <option value="desc">리뷰 높은순</option>
+          <option value="asc">리뷰 낮은순</option>
+        </select>
+      </div>
+
       <ReviewList reviewList={data.reviews} />
       <Pagination currentPage={data.currentPage} totalPages={data.totalPages} onClick={handlePageChange} />
     </div>
--- src/app/(common)/gathering/_hooks/useGetReviews.ts
+++ src/app/(common)/gathering/_hooks/useGetReviews.ts
@@ -1,10 +1,20 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { fetchReviews } from "../_utils/apis";
+import axiosInstance from "@/lib/axiosInstance";
+import type { ReviewQuery, ReviewsResponse } from "../types";
 
-export const useGetReviews = (gatheringId: string) => {
+export const getReviews = async (query?: ReviewQuery): Promise<ReviewsResponse> => {
+  try {
+    const { data } = await axiosInstance.get("/reviews", { params: query });
+    return data;
+  } catch (error) {
+    throw new Error("데이터를 불러오지 못했습니다.");
+  }
+};
+
+export const useGetReviews = (gatheringId: string, query: ReviewQuery) => {
   return useSuspenseQuery({
-    queryKey: ["reviews", gatheringId],
-    queryFn: () => fetchReviews({ gatheringId }),
+    queryKey: ["reviews", gatheringId, query],
+    queryFn: () => getReviews({ gatheringId, ...query }),
     select: (data) => ({
       reviews: data.data.map((review) => ({
         id: review.id,
