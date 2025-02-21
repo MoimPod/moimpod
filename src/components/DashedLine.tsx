6b329b4e6d8fbadeb199c55570ed6bcdb3471f50
suzinxix <suzinxix@gmail.com>
2025-02-21 11:47:48 +0900

    fix: DashedLine 컴포넌트에 className props 추가

--- src/components/DashedLine.tsx
+++ src/components/DashedLine.tsx
@@ -1,5 +1,7 @@
-export default function DashedLine() {
+export default function DashedLine({ className }: { className?: React.ReactNode }) {
   return (
-    <hr className="h-[2px] w-full border-none bg-[repeating-linear-gradient(90deg,#E5E7EB,#E5E7EB_5px,transparent_5px,transparent_10px)]" />
+    <hr
+      className={`h-[2px] w-full border-none bg-[repeating-linear-gradient(90deg,#E5E7EB,#E5E7EB_5px,transparent_5px,transparent_10px)] ${className}`}
+    />
   );
 }
