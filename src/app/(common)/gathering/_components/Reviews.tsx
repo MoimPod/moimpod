1d617ed3c9b69bfc174834a7ede253b47103c7ec
suzinxix <suzinxix@gmail.com>
2025-02-21 11:09:53 +0900

    fix: 필터링 최신순 수정

--- src/app/(common)/gathering/_components/Reviews.tsx
+++ src/app/(common)/gathering/_components/Reviews.tsx
@@ -1,46 +1,69 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import ReviewList from "../_components/ReviewList";
 import Pagination from "../_components/Pagination";
 import { useGetReviews } from "../_hooks/useGetReviews";
 import { useQueryParams } from "@/hooks/useQueryParams";
 
+const SORT = {
+  latest: "latest",
+  scoreDesc: "score-desc",
+  scoreAsc: "score-asc",
+};
+
 export default function Reviews({ gatheringId }: { gatheringId: string }) {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
   const paramsObj = useQueryParams();
-  const sortOrder = searchParams.get("sortOrder") || "latest";
-
+  const params = new URLSearchParams(searchParams.toString());
   const { data } = useGetReviews(gatheringId, paramsObj);
 
-  const handlePageChange = (page: number) => {
-    const newSearchParams = new URLSearchParams(searchParams.toString());
+  const [filter, setFilter] = useState(SORT.latest);
 
+  const handlePageChange = (page: number) => {
     const limit = 4;
     const offset = (page - 1) * limit;
 
-    newSearchParams.set("offset", offset.toString());
-    newSearchParams.set("limit", limit.toString());
+    params.set("offset", offset.toString());
+    params.set("limit", limit.toString());
 
-    router.push(`${pathname}?${newSearchParams.toString()}`);
+    router.push(`${pathname}?${params.toString()}`);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
-    const params = new URLSearchParams(searchParams.toString());
 
-    if (value === "latest") {
-      params.delete("sortBy");
-      params.delete("sortOrder");
+    if (value === SORT.latest) {
+      params.set("sortBy", "createdAt");
+      params.set("sortOrder", "desc");
+    } else if (value === SORT.scoreAsc) {
+      params.set("sortBy", "score");
+      params.set("sortOrder", "asc");
     } else {
       params.set("sortBy", "score");
-      params.set("sortOrder", value);
+      params.set("sortOrder", "desc");
     }
+
+    setFilter(value);
 
     router.push(`${pathname}?${params.toString()}`);
   };
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    // sortBy, sortOrder가 없으면 기본값을 추가
+    if (!params.has("sortBy") || !params.has("sortOrder")) {
+      params.set("sortBy", "createdAt");
+      params.set("sortOrder", "desc");
+    }
+
+    // 현재 URL을 기본 정렬값이 적용된 URL로 변경
+    router.replace(`${pathname}?${params.toString()}`);
+  }, []);
 
   if (data.reviews.length === 0) {
     return <div>리뷰가 존재하지 않습니다.</div>;
@@ -51,10 +74,10 @@ export default function Reviews({ gatheringId }: { gatheringId: string }) {
       <div className="flex justify-between">
         <h1 className="text-lg font-semibold">이용자들은 이 프로그램을 이렇게 느꼈어요!</h1>
 
-        <select value={sortOrder} onChange={handleChange}>
+        <select value={filter} onChange={handleChange}>
           <option value="latest">최신순</option>
-          <option value="desc">리뷰 높은순</option>
-          <option value="asc">리뷰 낮은순</option>
+          <option value="score-desc">리뷰 높은순</option>
+          <option value="score-asc">리뷰 낮은순</option>
         </select>
       </div>
 
