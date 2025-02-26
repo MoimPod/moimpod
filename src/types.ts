1c93c28e06b0d73cf10bad07138475ea288a4890
suzinxix <suzinxix@gmail.com>
2025-02-26 17:42:34 +0900

    feat: 모든 리뷰 무한 스크롤 구현

---
+++ src/types.ts
@@ -0,0 +1,52 @@
+export type UserType = {
+  id: number;
+  name: string;
+  email?: string;
+  companyName?: string;
+  image: string | null;
+};
+
+export type GatheringType = {
+  teamId: string;
+  id: number;
+  type: string;
+  name: string;
+  dateTime: string;
+  registrationEnd: string;
+  location: string;
+  participantCount: number;
+  image: string;
+  capacity: number;
+  createdBy: number;
+  canceledAt: string;
+};
+
+export type ReviewQuery = {
+  gatheringId?: string;
+  userId?: number;
+  type?: string;
+  location?: string;
+  date?: string;
+  registrationEnd?: string;
+  sortBy?: "createdAt" | "score" | "participantCount";
+  sortOrder?: "asc" | "desc";
+  limit?: number | string;
+  offset?: number | string;
+};
+
+export type ReviewResponse = {
+  teamId?: string;
+  id: number;
+  score: number;
+  comment: string;
+  createdAt: string;
+  Gathering?: GatheringType;
+  User: UserType;
+};
+
+export type ReviewsResponse = {
+  data: ReviewResponse[];
+  totalItemCount: number;
+  currentPage: number;
+  totalPages: number;
+};
