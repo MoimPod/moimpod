94354c5db377f119ba17ffbfaaf7299160585049
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2025-02-28 17:50:20 +0900

    fix: chipInfo 페이지 day js로 변경

--- src/components/ChipInfo.tsx
+++ src/components/ChipInfo.tsx
@@ -1,33 +1,24 @@
+import dayjs from "dayjs";
+
 type ChipInfoProps = {
   dateTime: string;
 };
 
 export default function ChipInfo({ dateTime }: ChipInfoProps) {
-  // 날짜를 "MM-DD" 형식으로 변환하는 함수
-  const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    const month = String(date.getMonth() + 1);
-    const day = String(date.getDate());
-    return `${month}월 ${day}일`;
-  };
-
-  // 시간를 "HH:MM" 형식으로 변환하는 함수
-  const formatTime = (dateString: string) => {
-    const date = new Date(dateString);
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    return `${hours}:${minutes}`;
-  };
+  // 날짜 포맷: "MM-DD" 형식
+  const formattedDate = dayjs(dateTime).format("M월 D일");
+  // 시간 포맷: "HH:MM" 형식
+  const formattedTime = dayjs(dateTime).format("HH:mm");
 
   return (
     <div className="flex gap-2">
       <div className="flex h-6 items-center justify-center rounded bg-gray-900 px-2">
         {/* 날짜 */}
-        <div className="text-center text-sm font-medium text-white">{formatDate(dateTime)}</div>
+        <div className="text-center text-sm font-medium text-white">{formattedDate}</div>
       </div>
       <div className="flex h-6 items-center justify-center rounded bg-gray-900 px-2">
         {/* 시간 */}
-        <div className="text-center text-sm font-medium text-primary-color">{formatTime(dateTime)}</div>
+        <div className="text-center text-sm font-medium text-primary-color">{formattedTime}</div>
       </div>
     </div>
   );
