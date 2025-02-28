491bfc2f9cab757db973ddf0bd87bfced491b338
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2025-02-28 10:05:30 +0900

    fix: 찜한 모임 제목 글자 크기 변경

--- src/app/(common)/favorites/page.tsx
+++ src/app/(common)/favorites/page.tsx
@@ -67,8 +67,8 @@ export default function Page() {
       <div className="mb-5 flex gap-6 pt-8">
         <FavoritesLogo />
         <div>
-          <div className="mb-2 text-sm font-semibold text-gray-700">찜한 모임</div>
-          <div className="text-2xl font-semibold text-gray-900">마감되기 전에 지금 바로 참여해보세요👀</div>
+          <div className="mb-2 text-2xl font-semibold text-gray-900">찜한 모임</div>
+          <div className="text-sm font-semibold text-gray-700">마감되기 전에 지금 바로 참여해보세요 👀</div>
         </div>
       </div>
       <div className="px-6 pt-6">
