73dd5a50be966a8ec15a8d6c4d7d16276989b6ed
dohyeons <ddhhss0603@gmail.com>
2025-03-05 10:24:04 +0900

    fix: axiosInstance 되돌리기 #156

--- src/lib/getServerToken.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { cookies } from "next/headers";
-
-export const getServerToken = async () => {
-  const cookie = await cookies();
-  const token = cookie.get("token")?.value;
-  return Promise.resolve(token);
-};
