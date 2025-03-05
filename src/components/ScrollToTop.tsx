bb645453ef4bdf72ee42d822945a9e69e2336041
dohyeons <ddhhss0603@gmail.com>
2025-03-05 17:23:09 +0900

    feat: ScrollToTop 버튼 구현 #191

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import ScrollArrow from "@/images/dropdown_down_arrow_white.svg";
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 400);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scroll({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <button
+      className={`fixed bottom-3 right-3 flex size-8 transform items-center justify-center rounded-full bg-blue-6 transition-all duration-300 ease-in-out md:size-12 ${
+        isVisible ? "scale-100 opacity-100" : "scale-0 opacity-0"
+      }`}
+      onClick={scrollToTop}
+    >
+      <ScrollArrow className="rotate-180" />
+    </button>
+  );
+}
