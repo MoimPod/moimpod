df574d68160c2fbd86ed525679c4f72eecb7e748
dohyeons <ddhhss0603@gmail.com>
2025-03-06 18:46:40 +0900

    fix: axios사용으로 변경 #202

--- src/app/(common)/mypage/page.tsx
+++ src/app/(common)/mypage/page.tsx
@@ -5,6 +5,7 @@ import axiosInstance from "@/lib/axiosInstance";
 import getQueryClient from "@/lib/getQueryClient";
 import { UserType } from "@/types";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import axios from "axios";
 
 import dayjs from "dayjs";
 import { cookies } from "next/headers";
@@ -13,13 +14,12 @@ export default async function Page() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
   const queryClient = getQueryClient();
-  axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
   await queryClient.prefetchQuery({
     queryKey: ["user"],
     queryFn: async () => {
       if (!token) return null;
-      const response = await axiosInstance.get<UserType>("auths/user", {
+      const response = await axios.get<UserType>("auths/user", {
         headers: { Authorization: `Bearer ${token}` },
       });
       return response.data;
@@ -30,7 +30,7 @@ export default async function Page() {
     queryKey: ["user", "gatherings", "joined"],
     queryFn: async () => {
       if (!token) return [];
-      const response = await axiosInstance.get<MyGathering[]>("gatherings/joined", {
+      const response = await axios.get<MyGathering[]>("gatherings/joined", {
         params: { limit: 100 },
         headers: { Authorization: `Bearer ${token}` },
       });
