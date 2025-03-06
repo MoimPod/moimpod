18aa8cd45741f37d398ad4cd1db306fe55e41fdf
dohyeons <ddhhss0603@gmail.com>
2025-03-06 16:24:54 +0900

    fix: HydrationBoundary 위치 변경 #202

--- src/app/(common)/mypage/page.tsx
+++ src/app/(common)/mypage/page.tsx
@@ -35,12 +35,12 @@ export default async function Page() {
 
   return (
     <div className="flex flex-1 flex-col gap-[30px] pt-8">
+      <div className="flex flex-col gap-6">
+        <h2 className="text-2xl font-semibold">마이 페이지</h2>
+        <ProfileSection />
+      </div>
+      {/* 마이페이지의 콘텐츠 */}
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="flex flex-col gap-6">
-          <h2 className="text-2xl font-semibold">마이 페이지</h2>
-          <ProfileSection />
-        </div>
-        {/* 마이페이지의 콘텐츠 */}
         <MypageContent />
       </HydrationBoundary>
     </div>
