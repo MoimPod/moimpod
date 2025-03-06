063b7499a65d30bec6b6739762d94ad60db94a63
dohyeons <ddhhss0603@gmail.com>
2025-03-06 21:45:55 +0900

    fix: axios로 변경 #202

--- src/app/(common)/mypage/page.tsx
+++ src/app/(common)/mypage/page.tsx
@@ -1,12 +1,10 @@
 import MypageContent from "@/app/(common)/mypage/_components/MypageContent";
 import { ProfileSection } from "@/app/(common)/mypage/_components/ProfileSection";
 import { MyGathering } from "@/app/(common)/mypage/types";
-import axiosInstance from "@/lib/axiosInstance";
 import getQueryClient from "@/lib/getQueryClient";
-import { UserType } from "@/types";
+import { GatheringType, ReviewsResponse, UserType } from "@/types";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import axios from "axios";
-
 import dayjs from "dayjs";
 import { cookies } from "next/headers";
 
@@ -14,38 +12,90 @@ export default async function Page() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
   const queryClient = getQueryClient();
-
   await queryClient.prefetchQuery({
     queryKey: ["user"],
     queryFn: async () => {
-      if (!token) return null;
-      const response = await axios.get<UserType>("auths/user", {
-        headers: { Authorization: `Bearer ${token}` },
+      const response = await axios.get<UserType>(`${process.env.NEXT_PUBLIC_API_BASE_URL}auths/user`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
       return response.data;
     },
   });
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: ["user", "gatherings", "joined"],
+      queryFn: async () => {
+        const response = await axios.get<MyGathering[]>(`${process.env.NEXT_PUBLIC_API_BASE_URL}gatherings/joined`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            limit: 100,
+          },
+        });
 
-  await queryClient.prefetchQuery({
-    queryKey: ["user", "gatherings", "joined"],
-    queryFn: async () => {
-      if (!token) return [];
-      const response = await axios.get<MyGathering[]>("gatherings/joined", {
-        params: { limit: 100 },
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      return response.data.sort((a, b) => dayjs(b.dateTime).valueOf() - dayjs(a.dateTime).valueOf());
-    },
-  });
+        return response.data.sort((a, b) => dayjs(b.dateTime).valueOf() - dayjs(a.dateTime).valueOf());
+      },
+    }),
 
+    queryClient.prefetchQuery({
+      queryKey: ["user", "reviews", "reviewable"],
+      queryFn: async () => {
+        const response = await axios.get<MyGathering[]>(`${process.env.NEXT_PUBLIC_API_BASE_URL}gatherings/joined`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            limit: 100,
+            completed: true,
+            reviewed: false,
+          },
+        });
+        const data = response.data.filter((gathering) => !gathering.canceledAt);
+        return data.sort((a, b) => dayjs(b.dateTime).valueOf() - dayjs(a.dateTime).valueOf());
+      },
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ["user", "reviews", "written"],
+      queryFn: async () => {
+        const user = queryClient.getQueryData<UserType>(["user"]);
+        const response = await axios.get<ReviewsResponse>(`${process.env.NEXT_PUBLIC_API_BASE_URL}reviews`, {
+          params: {
+            limit: 100,
+            userId: user?.id,
+          },
+        });
+
+        return response.data.data.sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+      },
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ["user", "gatherings", "created"],
+      queryFn: async () => {
+        const user = queryClient.getQueryData<UserType>(["user"]);
+        const response = await axios.get<GatheringType[]>(`${process.env.NEXT_PUBLIC_API_BASE_URL}gatherings`, {
+          params: {
+            limit: 100,
+            sortBy: "dateTime",
+            sortOrder: "desc",
+            createdBy: user?.id,
+          },
+        });
+
+        return response.data;
+      },
+    }),
+  ]);
   return (
     <div className="flex flex-1 flex-col gap-[30px] pt-8">
-      <div className="flex flex-col gap-6">
-        <h2 className="text-2xl font-semibold">마이 페이지</h2>
-        <ProfileSection />
-      </div>
-      {/* 마이페이지의 콘텐츠 */}
       <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="flex flex-col gap-6">
+          <h2 className="text-2xl font-semibold">마이 페이지</h2>
+          <ProfileSection />
+        </div>
+        {/* 마이페이지의 콘텐츠 */}
         <MypageContent />
       </HydrationBoundary>
     </div>
